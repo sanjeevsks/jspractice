9972e678afc6a9b79e77dae4eb33f453de90a9aa
Sanjeev <101730605+August-21@users.noreply.github.com>
2024-01-27 15:37:25 +0530

    jspractise-1

---
+++ landscape-portrait.js
@@ -0,0 +1,18 @@
+function is_landscape(l, b)
+{
+    let rectangle;
+    if(l>b)
+    {
+        rectangle="landscape";
+    }
+    else
+    {
+        rectangle="Portrait";
+    }
+    return(rectangle);
+}
+
+let l=20;
+let b=30;
+let rectangle=is_landscape(l,b);
+console.log(rectangle);
