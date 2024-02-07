62c6364f2b58e676c0ca1e3a9767e91716a290f6
Sanjeev <101730605+August-21@users.noreply.github.com>
2024-02-08 00:12:42 +0530

    added program information

--- landscape-portrait.js
+++ landscape-portrait.js
@@ -1,18 +1,22 @@
-function is_landscape(l, b)
-{
+/**
+ * 
+ * @author: Sanjeev Shukla
+ * @program: To find if the rectangle is portrait or landscape
+ * 
+ */
+
+function is_landscape(l, b) {
     let rectangle;
-    if(l>b)
-    {
-        rectangle="landscape";
+    if (l > b) {
+        rectangle = "landscape";
     }
-    else
-    {
-        rectangle="Portrait";
+    else {
+        rectangle = "Portrait";
     }
-    return(rectangle);
+    return (rectangle);
 }
 
-let l=20;
-let b=30;
-let rectangle=is_landscape(l,b);
+let l = 20;
+let b = 30;
+let rectangle = is_landscape(l, b);
 console.log(rectangle);
